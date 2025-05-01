c0d847ac7cbd2a860b54aafba079075285a4011b
Sejun Park <pkt369@naver.com>
2025-05-01 16:01:41 -0600

    add next config for deploying

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }, // 이미지 최적화 기능 제거
+  trailingSlash: true,           // 각 경로 뒤에 `/` 붙여야 GitHub Pages에서 잘 동작
+};
 
 export default nextConfig;
